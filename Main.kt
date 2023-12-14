be04fa290d8e5e433af8db57f4c778244b5ee60d
min <151513470+tkfkadldy@users.noreply.github.com>
2023-12-14 20:53:05 +0900

    test

---
+++ Main.kt
@@ -0,0 +1,60 @@
+
+/*fun String.isNumeric(): Boolean {
+    return try {
+        this.toInt()
+        true
+    } catch (e: Exception) {
+        false
+    }
+}*/
+
+
+fun main() {
+
+
+
+
+    /*while (true) {
+        println("[ Orders ]")
+        println("[메뉴]\n1. 주문 2. 주문 취소 3. 종료")
+
+        val menu = readln()
+        if (!menu.isNumeric()){
+            System.err.println("메뉴 입력은 숫자만 가능합니다")
+            continue
+        }
+
+        when(menu.toInt()){
+            1 -> {
+
+                println("주문해주세요")
+                println("----------------카페 키오스크----------------")
+                println("[메뉴]\n1. 아메리카노 2. 카페라떼 3. 카라멜마끼아또")
+                val num =  readln()
+
+                when(num.toInt()){
+                    1 -> {
+                        println("아메리카노를 구매하시겠습니까? [메뉴]\n" +
+                                "1. 예 2. 아니요")
+                        val ox = readln()
+                        if(ox == "1"){
+                            println("잠시만 기다려주시면 만들어드리겠습니다")
+
+                        }
+
+                    }
+
+                }
+                continue
+            }
+        }
+        //break
+
+
+
+    }*/
+
+
+}
+
+
