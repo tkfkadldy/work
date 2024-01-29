de16fa3082c367a3f4a32bd40ce9c48916f95bf7
tkfadldy <kimmin20012@gmail.com>
2024-01-29 17:20:44 +0900

    work 작성

--- src/main/kotlin/com/test/work/domain/work/dto/CreateWorkRequest.kt
+++ src/main/kotlin/com/test/work/domain/work/dto/CreateWorkRequest.kt
@@ -1,6 +1,8 @@
 package com.test.work.domain.work.dto
 
 data class CreateWorkRequest(
+    val userId:Long,
     val title:String,
-    val content: String
+    val content: String,
+    val name : String
 )
--- src/main/kotlin/com/test/work/domain/work/dto/UpdateWorkRequest.kt
+++ src/main/kotlin/com/test/work/domain/work/dto/UpdateWorkRequest.kt
@@ -3,4 +3,5 @@ package com.test.work.domain.work.dto
 data class UpdateWorkRequest(
     val title:String,
     val content: String,
+
 )
