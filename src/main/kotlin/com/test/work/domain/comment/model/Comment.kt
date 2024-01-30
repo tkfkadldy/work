dd332dab37ebf8f4c2652740c3621c450e69a852
tkfadldy <kimmin20012@gmail.com>
2024-01-30 12:59:47 +0900

    test

--- src/main/kotlin/com/test/work/domain/comment/model/Comment.kt
+++ src/main/kotlin/com/test/work/domain/comment/model/Comment.kt
@@ -11,12 +11,9 @@ class Comment (
     @Column(name = "comment", nullable = false)
     var comment: String,
 
-    //댓글이 속한 게시물을 나타내며, `Post` 엔티티와 다대일 관계
     @ManyToOne
     @JoinColumn(name = "workid")
     val work: Work,
-
-    //댓글을 작성한 사용자를 나타내며, `User` 엔티티와 다대일 관계
 
     @ManyToOne
     @JoinColumn(name = "userid")
